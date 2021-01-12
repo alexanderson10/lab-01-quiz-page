348696ca11fd896d4e34f8441014b16c50bc1a9e
Alexanderson10 <alexanderson1010@me.com>
2021-01-11 17:26:07 -0800

    first commit

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 // import functions and grab DOM elements
+import { countAsAYes } from './utils.js';
 const quizButton = document.getElementById('start-quiz');
+const hiddenDiv = document.getElementById('secret-div')
 // initialize state
 
 // set event listeners to update state and DOM
 quizButton.addEventListener('click', () => {
     alert('Welcome to my quiz!');
     const confirmation = confirm('Do you wish to proceed');
-    if (confirmation === false) {
-        return;
+    if (!confirmation) return;
+
+    let correctAnswers = 0;
+    const userFirstName = prompt('what is your first name');
+    const userLastName = prompt('what is your last name');
+    const firstName = prompt('is my first name Alex?');
+    if (countAsAYes(firstName)) correctAnswers++
+    const lastName = prompt('is my last name Anderson?');
+    if (countAsAYes(lastName)) correctAnswers++
+    const favoriteHobby = prompt('is my favorite hobby baseball growing up');
+    if (countAsAYes(favoriteHobby)) correctAnswers++
+    console.log(correctAnswers)
+
+    const resultsString = `Hey ${userFirstName} ${userLastName}! You got ${correctAnswers} correct!`;
+    console.log(resultsString)
 
 
-    }
+
 })
